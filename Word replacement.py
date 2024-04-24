d1ca0c7b1ab941e733d9dcc21fc7f1b7d49feff4
RaksithSivakumar <167526590+RaksithSivakumar@users.noreply.github.com>
2024-04-24 11:10:05 +0530

    Interactive Word Replacement Tool!
     Seamlessly enhance your text with targeted word substitutions, revolutionizing the way you write!

---
+++ Word replacement.py
@@ -0,0 +1,7 @@
+def replace_word():
+    my_str = "hello Connections"  
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(my_str.replace(word_to_replace, word_replacement))
+
+replace_word()  
