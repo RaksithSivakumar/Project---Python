f2498533476e1aa62ad620c227157eb8dcca5866
RaksithSivakumar <167526590+RaksithSivakumar@users.noreply.github.com>
2024-04-28 19:17:20 +0530

    Generate Number Patterns!
    Generating number patterns using nested loops.

---
+++ Pyramid Pattern.py
@@ -0,0 +1,20 @@
+
+def numpat(n):
+   
+    num = 1
+
+  
+    for i in range(0, n):
+       
+        num = 1
+
+        for j in range(0, i+1):
+
+            print(num, end=" ")
+
+            num = num + 1
+
+        print("\r")
+
+n = int(input("Enter the number of rows: "))
+numpat(n)
