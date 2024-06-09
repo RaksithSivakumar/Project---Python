d3c54707f8397fece363f3c517823cb2b46963a2
RaksithSivakumar <167526590+RaksithSivakumar@users.noreply.github.com>
2024-06-09 18:31:09 +0530

    Age Calculator Using Python Tkinter
    Age Calculator: Python Tkinter application that accurately calculates users' ages based on birthdate inputs.

---
+++ Age Calculator.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from datetime import date
+
+def calculateAge():
+    today = date.today()
+    birthDate = date(int(yearEntry.get()), int(monthEntry.get()), int(dayEntry.get()))
+    age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
+    result.config(text=f"{nameValue.get()}, your age is {age} years.")
+
+root = Tk()
+root.geometry("800x600")
+root.resizable(False, False)
+root.title("Age Calculator")
+
+Label(root, text = "Name", font=23).place(x=200, y=250)
+Label(root, text = "Year", font=23).place(x=200, y=300)
+Label(root, text = "Month", font=23).place(x=200, y=350)
+Label(root, text = "Day", font=23).place(x=200, y=400)
+
+nameValue = StringVar()
+yearValue = StringVar()
+monthValue = StringVar()
+dayValue = StringVar()
+
+nameEntry = Entry(root, textvariable=nameValue, width=30, bd=3, font=20)
+nameEntry.place(x=300, y=250)
+
+yearEntry = Entry(root, textvariable=yearValue, width=30, bd=3, font=20)
+yearEntry.place(x=300, y=300)
+
+monthEntry = Entry(root, textvariable=monthValue, width=30, bd=3, font=20)
+monthEntry.place(x=300, y=350)
+
+dayEntry = Entry(root, textvariable=dayValue, width=30, bd=3, font=20)
+dayEntry.place(x=300, y=400)
+
+Button(root, text="Calculate Age", font=20, bg="black", fg="white", width=11, height=2, command=calculateAge).place(x=300, y=450)
+
+result = Label(root, text="", font=23)
+result.place(x=300, y=500)
+
+root.mainloop()
